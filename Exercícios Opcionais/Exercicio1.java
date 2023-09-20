9de6740dbd11bd3c8ab566053270d1b337766a02
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-09-20 14:26:39 -0300

    Update Exercicio1.java

--- Exercícios Opcionais/Exercicio1.java
+++ Exercícios Opcionais/Exercicio1.java
@@ -9,28 +9,28 @@ import java.util.Scanner;
 
 public class Exercicio1 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int x, y, soma;
+        int x, y, soma;
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println(" ===== Somatório de Valores =====\n");
+        System.out.println(" ===== Somatório de Valores =====\n");
 
-		System.out.print(" > Insira o primeiro valor: ");
+        System.out.print(" > Insira o primeiro valor: ");
 
-		x = sc.nextInt(); System.out.println();
+        x = sc.nextInt(); System.out.println();
 
-		System.out.print(" > Insira o segundo valor: ");
+        System.out.print(" > Insira o segundo valor: ");
 
-		y = sc.nextInt(); System.out.println();
+        y = sc.nextInt(); System.out.println();
 
-		soma = x + y;
+        soma = x + y;
 
-		System.out.println(" O valor da soma é: " + soma);
+        System.out.println(" O valor da soma é: " + soma);
 
-		sc.close();
+        sc.close();
 
-	}
+    }
 
 }
