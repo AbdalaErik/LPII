4d6b83468e49cf8f1f8ca3a63466f70b2a30b145
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-09-20 14:30:20 -0300

    Update Exercicio4.java

--- Exercícios Opcionais/Exercicio4.java
+++ Exercícios Opcionais/Exercicio4.java
@@ -15,51 +15,51 @@ public class Exercicio4 {
         
     }
     
-	public static void main(String[] args) {
-	    
-	    Vector<Integer> idFuncionario = new Vector<Integer>(2);
-	    Vector<Double> HorasTrabalhadas = new Vector<Double>(2);
-	    Vector<Double> ValorPorHora = new Vector<Double>(2);
-	    Vector<Double> Salario = new Vector<Double>(2);
-	    
-	    Scanner sc = new Scanner(System.in);
-	    
-	    System.out.println(" ================== Informações do Funcionário ==================\n");
-	    
+    public static void main(String[] args) {
+        
+        Vector<Integer> idFuncionario = new Vector<Integer>(2);
+        Vector<Double> HorasTrabalhadas = new Vector<Double>(2);
+        Vector<Double> ValorPorHora = new Vector<Double>(2);
+        Vector<Double> Salario = new Vector<Double>(2);
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(" ================== Informações do Funcionário ==================\n");
+        
         for (int i = 0; i < 2; i++) {
             
             System.out.println(" # Inserção de informações para o " + (i + 1) + "º funcionário\n");
 
-	        System.out.print(" > Insira o número de identificação do funcionário (ID): ");
-	    
-	        idFuncionario.add(sc.nextInt()); System.out.println();
-	    
-	        System.out.print(" > Insira o número de horas trabalhadas: ");
-	    
-	        HorasTrabalhadas.add(sc.nextDouble()); System.out.println();
-	    
-	        System.out.print(" > Insira o valor recebido por hora (R$): ");
-	    
-	        ValorPorHora.add(sc.nextDouble()); System.out.println();
-	    
-	        Salario.add(Calculo(HorasTrabalhadas.get(i), ValorPorHora.get(i)));
-	        
-	        System.out.println("-----------------------------------------------------------------\n");
+            System.out.print(" > Insira o número de identificação do funcionário (ID): ");
+        
+            idFuncionario.add(sc.nextInt()); System.out.println();
+        
+            System.out.print(" > Insira o número de horas trabalhadas: ");
+        
+            HorasTrabalhadas.add(sc.nextDouble()); System.out.println();
+        
+            System.out.print(" > Insira o valor recebido por hora (R$): ");
+        
+            ValorPorHora.add(sc.nextDouble()); System.out.println();
+        
+            Salario.add(Calculo(HorasTrabalhadas.get(i), ValorPorHora.get(i)));
+            
+            System.out.println("-----------------------------------------------------------------\n");
 
-			System.out.println(" # Resultados para o " + (i + 1) + "º funcionário\n");
-	    
-	        System.out.println(" O número de identificação do funcionário é: " + idFuncionario.get(i) + "\n");
-	    
-	        System.out.println(" O salário do funcionário é: R$ " + String.format("%.2f", Salario.get(i)));
-	        
-	        if (i == 0) System.out.println();
-	        
-	        if (i != 1) System.out.println("_________________________________________________________________\n");
-	        
+            System.out.println(" # Resultados para o " + (i + 1) + "º funcionário\n");
+        
+            System.out.println(" O número de identificação do funcionário é: " + idFuncionario.get(i) + "\n");
+        
+            System.out.println(" O salário do funcionário é: R$ " + String.format("%.2f", Salario.get(i)));
+            
+            if (i == 0) System.out.println();
+            
+            if (i != 1) System.out.println("_________________________________________________________________\n");
+            
         }
-	    
+        
         sc.close();
-		
-	}
-	
+        
+    }
+    
 }
