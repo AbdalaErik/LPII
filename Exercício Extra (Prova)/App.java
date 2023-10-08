2b7fcfd438f1be67f5f28800920d90ef8890824b
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-10-08 18:17:25 -0300

    Create App.java

---
+++ Exercício Extra (Prova)/App.java
@@ -0,0 +1,23 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        
+        // Crie o objeto motoHonda da classe moto:
+        
+        moto motoHonda = new moto(150, "Honda CBR 1000RR", 2022, "Vermelha", 45000.00, 17, "Michelin Pilot Road 4");
+        
+        // Crie o objeto carroFiat da classe carro:
+        
+        carro carroFiat = new carro(180, "Fiat Palio", 2022, "Azul",  50000.00, 15, "Pirelli Cinturato P1");
+        
+        // Imprima os atributos do objeto motoHonda:
+        
+        motoHonda.Imprimir();
+        
+        // Imprima os atributos do objeto carroFiat:
+        
+        carroFiat.Imprimir();
+        
+    }
+    
+}
