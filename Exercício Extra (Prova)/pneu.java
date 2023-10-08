8b47f4f038fadf588df4c7c22875937f95b94c15
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-10-08 18:20:48 -0300

    Create pneu.java

---
+++ Exercício Extra (Prova)/pneu.java
@@ -0,0 +1,15 @@
+public class pneu {
+    
+    int aro;
+    String modelo;
+    
+    // Construtor:
+    
+    public pneu(int aro, String modelo) {
+        
+        this.aro = aro;
+        this.modelo = modelo;
+        
+    }
+    
+}
