3981b1bcd1f149e5127eca6ae33ea47b04bad0ec
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-10-13 22:38:23 -0300

    Update pneu.java

--- Exercício Extra (Prova)/pneu.java
+++ Exercício Extra (Prova)/pneu.java
@@ -1,3 +1,10 @@
+// Exercício Extra (Prova) (3/5)
+// IFSULDEMINAS - Câmpus Muzambinho
+// Ciência da Computação - 4º Período (2023/2)
+// Linguagens de Programação II (LPII)
+// Docente: Fernanda Maria Ribeiro
+// Discente: Erik Bolonha Abdala
+
 public class pneu {
     
     int aro;
