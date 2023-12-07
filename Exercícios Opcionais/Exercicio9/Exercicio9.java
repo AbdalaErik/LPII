d35db2c77f268b687dd9a1a04e192f9ebee23de4
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-12-06 22:29:52 -0300

    Update Exercicio9.java

--- Exercícios Opcionais/Exercicio9/Exercicio9.java
+++ Exercícios Opcionais/Exercicio9/Exercicio9.java
@@ -12,6 +12,8 @@
 // de 2 objetos da classe Funcionario e 2 objetos da classe Cliente. E, além de ainda calcular o salário do funcionário, imprime
 // todos os atributos das classes por meio de métodos próprios.
 
+package Exercicio9;
+
 import java.util.*;
 
 public class Exercicio9 {
