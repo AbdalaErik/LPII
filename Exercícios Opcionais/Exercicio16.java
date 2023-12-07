82e1b272fd4dd649995c9229267b31637c7c6ad3
AbdalaErik <144078376+AbdalaErik@users.noreply.github.com>
2023-12-07 00:05:17 -0300

    Add files via upload

---
+++ Exercícios Opcionais/Exercicio16.java
@@ -0,0 +1,98 @@
+// Lista de Exercícios Opcionais - Exercício 16
+// IFSULDEMINAS - Câmpus Muzambinho
+// Ciência da Computação - 4º Período (2023/2)
+// Linguagens de Programação II (LPII)
+// Docente: Fernanda Maria Ribeiro
+// Discente: Erik Bolonha Abdala
+
+abstract class Animal {
+
+    abstract void fazerSom();
+
+}
+
+class Passaro extends Animal {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("O pássaro faz vários sons.\n");
+
+    }
+
+}
+
+class Mamifero extends Animal {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("O mamífero faz vários sons.\n");
+
+    }
+
+}
+
+class BemTeVi extends Passaro {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("O Bem-te-vi pia.\n");
+
+    }
+
+}
+
+class Papagaio extends Passaro {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("O papagaio fala.\n");
+
+    }
+
+}
+
+class Cachorro extends Mamifero {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("O cachorro late.\n");
+
+    }
+
+}
+
+class Vaca extends Mamifero {
+
+    @Override
+    public void fazerSom() {
+
+        System.out.println("A vaca muge.\n");
+
+    }
+
+}
+
+public class Exercicio16 {
+
+    public static void main(String[] args) {
+
+        Passaro passaro = new Passaro(); passaro.fazerSom();
+
+        Mamifero mamifero = new Mamifero(); mamifero.fazerSom();
+
+        BemTeVi bem_te_vi = new BemTeVi(); bem_te_vi.fazerSom();
+
+        Papagaio papagaio = new Papagaio(); papagaio.fazerSom();
+
+        Cachorro cachorro = new Cachorro(); cachorro.fazerSom();
+
+        Vaca vaca = new Vaca(); vaca.fazerSom();
+
+    }
+
+}
